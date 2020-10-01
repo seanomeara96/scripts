6ba181d376a8c0998f85e406d292f4f5f1b494ac
Sean <seanom96@gmail.com>
2020-10-01 21:04:40 +0100

    takes command line arguments

--- ils-mkdir.py
+++ ils-mkdir.py
@@ -4,12 +4,22 @@ import sys
 brands = ["beautyfeatures", "inhealth", "babysafety", "beautyskincare",
           "allhair", "pregnancyandbaby", "fertilitystore", "haakaa"]
 
-# absolute path to current directory
-parent_path = os.path.abspath(".")
+# path to new directory
+path_to_new_directory = sys.argv[1]
+
+# name of new directory
+new_dir_name = sys.argv[2]
+
+# join to make new directory to add brand directories to
+new_dir_path = os.path.join(path_to_new_directory, new_dir_name)
+
+# make new parent directory
+os.mkdir(new_dir_path)
+
 
 # create new directory for each brand
 for x in brands:
-    path = os.path.join(parent_path, x)
+    path = os.path.join(new_dir_path, x)
     os.mkdir(path)
 
 # Signal completion
