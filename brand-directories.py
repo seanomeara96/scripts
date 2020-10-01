1414f8a99c676f23a98df7c7ebfa39c7e6f5b77f
Sean <seanom96@gmail.com>
2020-10-01 20:33:40 +0100

    makes directories

--- brand-directories.py
+++ brand-directories.py
@@ -1,1 +1,15 @@
-brands = ["beautyfeatures", "inhealth", "babysafety", "beautyskincare", "allhair", "pregnancyandbaby", "fertilitystore", "haakaa"]
+import os
+# List of directory names I need to create
+brands = ["beautyfeatures", "inhealth", "babysafety", "beautyskincare",
+          "allhair", "pregnancyandbaby", "fertilitystore", "haakaa"]
+
+# absolute path to current directory
+parent_path = os.path.abspath(".")
+
+# create new directory for each brand
+for x in brands:
+    path = os.path.join(parent_path, x)
+    os.mkdir(path)
+
+# Signal completion
+print("Done.")
