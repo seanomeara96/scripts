40baa2332efcc0ae68166cdeed475ea2331a9d94
Sean <seanom96@gmail.com>
2020-10-10 09:49:34 +0100

    cwd -> path

--- img-rename.py
+++ img-rename.py
@@ -1,10 +1,13 @@
 import os
+import sys
 # programatically rename images
 # start with som, use time elapsed since last modification and the directory name
-current_working_dirname = os.getcwd().split("\\").pop()
-with os.scandir() as it:
+current_working_dirname = sys.argv[1].split("\\").pop()
+with os.scandir(sys.argv[1]) as it:
     for entry in it:
         if not entry.name.startswith(current_working_dirname) and entry.is_file():
             # print(os.path.join(os.getcwd(), f'{current_working_dirname}-{entry.stat().st_mtime}.{entry.name.split(".").pop()}'))
-            os.rename(r'{os.path.join(os.getcwd(), entry.name)}',
+            os.rename(r'{os.path.join(sys.argv[1], entry.name)}',
                       r'{current_working_dirname}-{entry.stat().st_mtime}.{entry.name.split(".").pop()}')
+
+# C:\Users\seano\Pictures
