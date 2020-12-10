fd3a193ec7010ea54d0041e5d5a7737b79498c78
Sean <seanom96@gmail.com>
2020-12-10 22:25:02 +0000

    csv formatter

--- formatcsv.py
+++ formatcsv.py
@@ -1,4 +1,17 @@
 import csv
-with open("beautyfeatures-09-Dec-2020.csv") as csv_file:
-    reader = csv.reader(x.replace("\0", "") for x in csv_file)
-    print(reader[0])
+'''
+This script takes the exported table data from 
+
+'''
+data = ""
+with open("beautyfeatures-09-Dec-2020.csv", "r", encoding="utf16", newline="") as csv_file:
+    my_file = csv.reader(csv_file, delimiter="\t")
+    line_count = 0
+    for line in my_file:
+        if line_count > 0:
+            for link in line[6].split("\n"):
+                if link.startswith("http://"):
+                    data += f"{line[1]}, {link}\n"
+        line_count += 1
+with open("formatted.csv", "w") as new_file:
+    new_file.write(data)
