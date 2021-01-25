9a15edd79670e4ac9bca373d7f45d918ecae86bd
Sean <seanom96@gmail.com>
2021-01-25 20:45:47 +0000

    checked links on homepage

--- test.js
+++ test.js
@@ -1,8 +1,34 @@
-function main(){
-    let status = 1
-    while(status){
-        //do this
+const axios = require("axios");
+const { JSDOM } = require("jsdom");
+let count = 0;
+let links;
+axios
+  .get("https://beautyfeatures.ie/")
+  .then((res) => {
+    let dom = new JSDOM(res.data);
+    links = new Array(...dom.window.document.querySelectorAll("a"));
+    links = links.map((a) => a.href);
+    logLinkInfo();
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+async function logLinkInfo() {
+  let href = links[count];
+  let res;
+  if (href.startsWith("http") || href.startsWith("/")) {
+    if (href.startsWith("/")) {
+      href = "https://www.beautyfeatures.ie" + href;
     }
-    return status
+    try {
+      res = await axios.get(href);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  if (count < links.length) {
+    console.log(count, href, res ? res.status : null);
+    count++;
+    logLinkInfo();
+  }
 }
-main()
