b6c0fe78eb78879e2f8a49d5ccca0c6eefc1cc68
Sean <seanom96@gmail.com>
2021-02-04 20:13:47 +0000

    some change

--- test.js
+++ test.js
@@ -1,9 +1,4 @@
 const parse = require("csv-parse/lib/sync");
 const fs = require("fs");
-fs.createReadStream("./products-2021-02-02.csv")
-  .on("readable", (row) => {
-    parse(row);
-  })
-  .on("end", () => {
-    console.log("processed");
-  });
+
+const parser = parse(fs.createReadStream("./products-2021-02-02.csv"));
