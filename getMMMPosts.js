84cb1292f7825098e3a6fae1e6214aff9bcc2629
Sean <seanom96@gmail.com>
2021-03-07 18:23:08 +0000

    update

--- getMMMPosts.js
+++ getMMMPosts.js
@@ -1,4 +1,6 @@
-const axios = require("axios"), fs = require("fs"), { JSDOM } = require("jsdom");
+const axios = require("axios"),
+  fs = require("fs"),
+  { JSDOM } = require("jsdom");
 let count = 0;
 /**
  * Gets posts from specified url and writes them to a file
@@ -15,7 +17,7 @@ function getPosts() {
 function writeFile({ data }) {
   fs.appendFile(
     "mmmPosts.txt",
-    [...new JSDOM({ data }).window.document.querySelectorAll(".post_box")]
+    [...new JSDOM(data).window.document.querySelectorAll(".post_box")]
       .map(
         (el) =>
           `${el.children[0].textContent}\t${el.children[2].textContent}\t${el.children[2].firstChild.href}\n`
