cb69f83fc5035f28e25ddbe4e5562b5f23fd4056
Sean <seanom96@gmail.com>
2021-03-30 20:18:58 +0100

    applied filters

---
+++ bf-filters/app.js
@@ -0,0 +1,18 @@
+const data = require("./bf-filters.json");
+const fs = require("fs");
+const filtersApplied = data.map((el) => {
+  let filters = [];
+  for (let key in el) {
+    if (el[key] == "TRUE") {
+      filters.push(`"Skin Concerns=${key}"`);
+    }
+  }
+  el.filters = filters.join(";");
+  return el;
+});
+
+console.log(filtersApplied);
+
+fs.writeFile("filtersApplied.json", JSON.stringify(filtersApplied), (err) => {
+  if (err) throw Error(err);
+});
