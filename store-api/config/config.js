23d10c3bb64870875b7208277119fe9d40aa5c14
seanomeara96 <seanom96@gmail.com>
2021-04-30 09:38:21 +0100

    includes bf & getAllProducts pulls from config

--- store-api/config/config.js
+++ store-api/config/config.js
@@ -2,12 +2,15 @@ const dotenv = require("dotenv");
 const path = require("path");
 dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 const config = {
-  xAuthTokenHeader: { "x-auth-token": process.env.XAUTHTOKEN },
+  fs_xAuthTokenHeader: returnTokenHeader(process.env.FS_XAUTHTOKEN),
+  bf_xAuthTokenHeader: returnTokenHeader(process.env.BF_XAUTHTOKEN),
   bf: process.env.BF_STORE_HASH,
   bsk: process.env.BSK_STORE_HASH,
   bs: process.env.BS_STORE_HASH,
   ah: process.env.AH_STORE_HASH,
   fs: process.env.FS_STORE_HASH,
 };
-
+function returnTokenHeader(authToken){
+  return { "x-auth-token": authToken }
+}
 module.exports = config;
