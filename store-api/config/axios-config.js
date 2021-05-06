dc64963278726ba065f50dbb449898a508f8720c
seanomeara96 <seanom96@gmail.com>
2021-05-06 09:49:22 +0100

    moved store-api to own repo

--- store-api/config/axios-config.js
+++
@@ -1,8 +0,0 @@
-const axios = require("axios");
-const config = require("./config");
-const fstore = axios.create({
-  baseURL: `https://api.bigcommerce.com/stores/${config.bf}/v3`,
-  headers: config.bf_xAuthTokenHeader,
-});
-
-module.exports = fstore;
--- store-api/filters/applyFilter.js
+++
@@ -1,9 +0,0 @@
-const { applyFilter } = require("./modules/create");
-
-applyFilter(178, "Proceive", "Men & Women")
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
